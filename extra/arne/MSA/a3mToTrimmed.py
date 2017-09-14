393bf8d1bcc9f319aa2d6152aca59f959f949db9
Arne Elofsson at Abisko <arne@bioinfo.se>
2017-09-14 16:00:10 +0200

    By syncing

--- extra/arne/MSA/a3mToTrimmed.py
+++ extra/arne/MSA/a3mToTrimmed.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys, os, argparse
 
-infilef = sys.argv[1]
-infile = open(infilef)
+
+parser = argparse.ArgumentParser(description="Trimming extra charcters in aligned sequence sfrom an a3m file")
+parser.add_argument('-o','--orgname', help='Keep original filenames', action="store_true")
+#parser.add_argument('file', metavar='file', type=argparse.FileType('r'), nargs=1, help='filename')
+parser.add_argument('file', metavar='file', type=str, nargs=1, help='filename')
+args = parser.parse_args()
+
+print args
+
+#if args.orgname:
+#        print "verbosity turned on"
+
+for infilef in args.file:
+    print infilef
+    infile = open(infilef)
+
+
+
 
 counter = 0
 for l in infile:
     if '>' in l and not counter == 0:
-        sys.stdout.write('\n>sequence{0:07d}/1-100\n'.format(counter))
+        if args.orgname:
+            sys.stdout.write('\n'+l)
+        else:
+            sys.stdout.write('\n>sequence{0:07d}/1-100\n'.format(counter))
         #sys.stdout.write('>sequence{0:07d}/1-100\n'.format(counter))
         counter += 1
     elif '>' not in l:
@@ -17,6 +36,10 @@ for l in infile:
         upperseq = upperseq.replace('X', '-')
         sys.stdout.write(upperseq)
     elif '>' in l and counter == 0:
-        sys.stdout.write('>target/1-100\n')
+        if args.orgname:
+            sys.stdout.write(l)
+        else:
+            sys.stdout.write('>target/1-100\n')
+
         counter += 1
 sys.stdout.write('\n')
